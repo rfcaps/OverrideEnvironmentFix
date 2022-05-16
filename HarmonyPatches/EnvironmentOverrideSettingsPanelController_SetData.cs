7a5fa7811af29dbd5df5e0466ae7969bacb88f23
rfcaps <rfcaps8@gmail.com>
2022-05-15 23:00:05 -0600

    Initial commit with working mod

---
+++ HarmonyPatches/EnvironmentOverrideSettingsPanelController_SetData.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+
+namespace OverrideEnvironmentFix.HarmonyPatches
+{
+    [HarmonyPatch(typeof(EnvironmentOverrideSettingsPanelController), nameof(EnvironmentOverrideSettingsPanelController.SetData))]
+    internal class EnvironmentOverrideSettingsPanelController_SetData
+    {
+        [UsedImplicitly]
+        internal static void Prefix(OverrideEnvironmentSettings overrideEnvironmentSettings) => overrideEnvironmentSettings.overrideEnvironments = true;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+﻿# OverrideEnvironmentFix
+A mod that will override environments by default again since Beat Games took that away.
